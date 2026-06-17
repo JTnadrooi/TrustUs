1e5162f188729ffe13eb018a3186a1cd72952dce
JTnadrooi <jtnadrooi@gmail.com>
2026-06-17 14:26:24 +0200

    Add logout (forgor)

--- src/header.php
+++ src/header.php
@@ -10,6 +10,15 @@
     <header>
         <h1><?php echo htmlspecialchars(SITE_NAME); ?></h1>
         With <i>all</i> your data.
+        <nav>
+            <!-- <a href="index.php">Home</a> | -->
+            <?php if (isLoggedIn()): ?>
+                <br />
+                <button type="button" onclick="location.href='logout.php'">
+                    Logout (<?php echo htmlspecialchars($_SESSION['username']); ?>)
+                </button>
+            <?php endif; ?>
+        </nav>
         <hr>
     </header>
 
